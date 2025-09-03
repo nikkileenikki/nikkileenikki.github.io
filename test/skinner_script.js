05fd98ed4721501db540a7a0f30c71007c2880af
Nikki Lee <nikki.lee.nikki@gmail.com>
2025-09-03 18:13:52 +0800

    Update skinner_script.js

--- test/skinner_script.js
+++ test/skinner_script.js
@@ -1,1 +1,24 @@
-console.log("Hello world");
+console.log("Git loaddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd")
+  setTimeout(() => {
+    function updateSize2() {
+      centerWidth = document.getElementById('center').offsetWidth;
+      centerHeight = document.getElementById('center').offsetHeight;
+      panelWidth = ((window.innerWidth*0.8) - centerWidth) / 2;
+      panelHeight = centerHeight + 500;
+      gsap.set(".left, .right", { width: panelWidth });
+      const leftPanelContent = document.querySelector(".left #image2");
+      const rightPanelContent = document.querySelector(".right #image3");
+      console.log(panelWidth /2);
+
+      let posTopPanel = panelWidth /2;
+      if(posTopPanel < 180){
+        leftPanelContent.style.top = panelWidth / 2+ "px";
+        rightPanelContent.style.top = panelWidth / 2+ "px";
+      }else{
+        leftPanelContent.style.top = "40px";
+        rightPanelContent.style.top = "40px";
+      }  console.log("resize");    
+    }
+  window.removeEventListener("resize", updateSize);
+  window.addEventListener("resize", updateSize2);
+  }, 3000);
