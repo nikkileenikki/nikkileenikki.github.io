7d541ce146b5b3fb8fc5d92b1b2d4971a452a2ea
Nikki Lee <nikki.lee.nikki@gmail.com>
2026-04-17 10:04:28 +0800

    Initialize ad builder with store and history

--- kult-adbuilder-dev/static/js/main.js
+++ kult-adbuilder-dev/static/js/main.js
@@ -1,1 +1,12 @@
+import { createStore } from './state/store.js';
+import { createBannerState } from './state/schema.js';
+import { createHistory } from './state/history.js';
+import { _log } from './core/debug.js';
 
+const store = createStore(createBannerState());
+const history = createHistory();
+
+window.adBuilderStore = store;
+window.adBuilderHistory = history;
+
+_log('Ad Builder Phase 1 bootstrap loaded');
