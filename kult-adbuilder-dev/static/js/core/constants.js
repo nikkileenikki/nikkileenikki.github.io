743cc2703d17d2affd0e5ad814ad3cbcf40ff73f
Nikki Lee <nikki.lee.nikki@gmail.com>
2026-04-17 09:58:36 +0800

    Add constants for banner version and element types

--- kult-adbuilder-dev/static/js/core/constants.js
+++ kult-adbuilder-dev/static/js/core/constants.js
@@ -1,1 +1,15 @@
+export const CURRENT_BANNER_VERSION = 1;
 
+export const ELEMENT_TYPES = {
+  TEXT: 'text',
+  IMAGE: 'image',
+  SHAPE: 'shape',
+  VIDEO: 'video',
+  CLICKTHROUGH: 'clickthrough',
+  INVISIBLE: 'invisible'
+};
+
+export const DEFAULT_BANNER_SIZE = {
+  width: 300,
+  height: 250
+};
