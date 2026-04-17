4775c03930d619cc792f4062759760b4f85cabf9
Nikki Lee <nikki.lee.nikki@gmail.com>
2026-04-17 10:04:06 +0800

    Update history.js

--- kult-adbuilder-dev/static/js/state/history.js
+++ kult-adbuilder-dev/static/js/state/history.js
@@ -1,1 +1,48 @@
+import { deepClone } from '../core/utils.js';
 
+export function createHistory(limit = 100) {
+  let undoStack = [];
+  let redoStack = [];
+
+  function push(state) {
+    undoStack.push(deepClone(state));
+    if (undoStack.length > limit) undoStack.shift();
+    redoStack = [];
+  }
+
+  function undo(currentState) {
+    if (!undoStack.length) return null;
+    const previous = undoStack.pop();
+    redoStack.push(deepClone(currentState));
+    return deepClone(previous);
+  }
+
+  function redo(currentState) {
+    if (!redoStack.length) return null;
+    const next = redoStack.pop();
+    undoStack.push(deepClone(currentState));
+    return deepClone(next);
+  }
+
+  function clear() {
+    undoStack = [];
+    redoStack = [];
+  }
+
+  function canUndo() {
+    return undoStack.length > 0;
+  }
+
+  function canRedo() {
+    return redoStack.length > 0;
+  }
+
+  return {
+    push,
+    undo,
+    redo,
+    clear,
+    canUndo,
+    canRedo
+  };
+}
