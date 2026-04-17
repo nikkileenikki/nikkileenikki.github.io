a78e3f1b6278e01fbaabf7a6ae5f85c8470dc7ad
Nikki Lee <nikki.lee.nikki@gmail.com>
2026-04-17 09:56:52 +0800

    Update debug.js

--- kult-adbuilder-dev/static/js/core/debug.js
+++ kult-adbuilder-dev/static/js/core/debug.js
@@ -1,1 +1,11 @@
+export function _log(...a) {
+  if (window.AD_BUILDER_DEBUG) console.log(...a);
+}
 
+export function _err(...a) {
+  if (window.AD_BUILDER_DEBUG) console.error(...a);
+}
+
+export function _warn(...a) {
+  if (window.AD_BUILDER_DEBUG) console.warn(...a);
+}
