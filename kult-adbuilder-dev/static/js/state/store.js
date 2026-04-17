37886b0a2ce99ef671cf4b5d0f1b7dad463bad64
Nikki Lee <nikki.lee.nikki@gmail.com>
2026-04-17 10:03:48 +0800

    Update store.js

--- kult-adbuilder-dev/static/js/state/store.js
+++ kult-adbuilder-dev/static/js/state/store.js
@@ -1,1 +1,34 @@
+import { createBannerState, normalizeBannerState } from './schema.js';
+import { deepClone } from '../core/utils.js';
 
+export function createStore(initialState = createBannerState()) {
+  let state = normalizeBannerState(initialState);
+  const listeners = new Set();
+
+  function getState() {
+    return state;
+  }
+
+  function setState(nextState) {
+    state = normalizeBannerState(nextState);
+    listeners.forEach(listener => listener(state));
+  }
+
+  function update(updater) {
+    const draft = deepClone(state);
+    const result = updater(draft);
+    setState(result || draft);
+  }
+
+  function subscribe(listener) {
+    listeners.add(listener);
+    return () => listeners.delete(listener);
+  }
+
+  return {
+    getState,
+    setState,
+    update,
+    subscribe
+  };
+}
