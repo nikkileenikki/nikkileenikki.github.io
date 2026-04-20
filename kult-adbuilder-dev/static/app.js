c117265088ff2cb7616ee7669458ec611b1b5eee
Nikki Lee <nikki.lee.nikki@gmail.com>
2026-04-20 13:55:55 +0800

    Refactor element label and action settings functions

--- kult-adbuilder-dev/static/app.js
+++ kult-adbuilder-dev/static/app.js
@@ -1473,54 +1473,23 @@
         hoverSelect.val(hoverValue);
     }
     
-    // Get human-readable label for element
     function getElementLabel(element) {
-        if (element.type === 'text') {
-            return `Text: ${element.text.substring(0, 15)}`;
-        } else if (element.type === 'clickthrough') {
-            const clickIndex = elements.filter(el => el.type === 'clickthrough').findIndex(el => el.id === element.id) + 1;
-            return `Click${clickIndex}`;
-        } else if (element.type === 'invisible') {
-            const invisibleIndex = elements.filter(el => el.type === 'invisible').findIndex(el => el.id === element.id) + 1;
-            return `Invisible${invisibleIndex}`;
-        } else if (element.type === 'shape') {
-            const shapeIndex = elements.filter(el => el.type === 'shape').findIndex(el => el.id === element.id) + 1;
-            return `Shape${shapeIndex}`;
-        } else if (element.type === 'video') {
-            return `Video: ${element.videoName}`;
-        } else {
-            return element.filename || 'Image';
-        }
+        return window.adBuilderRender.propertiesUI.getElementLabel({
+            element,
+            elements
+        });
     }
     
-    // Update click action-specific settings visibility
     function updateClickActionSettings(action) {
-        $('#clickShadowSettings').addClass('hidden');
-        $('#clickGlowSettings').addClass('hidden');
-        $('#clickScaleSettings').addClass('hidden');
-        
-        if (action === 'addShadow') {
-            $('#clickShadowSettings').removeClass('hidden');
-        } else if (action === 'addGlow') {
-            $('#clickGlowSettings').removeClass('hidden');
-        } else if (action === 'scale') {
-            $('#clickScaleSettings').removeClass('hidden');
-        }
+        return window.adBuilderRender.propertiesUI.updateClickActionSettings({
+            action
+        });
     }
     
-    // Update hover action-specific settings visibility
     function updateHoverActionSettings(action) {
-        $('#hoverShadowSettings').addClass('hidden');
-        $('#hoverGlowSettings').addClass('hidden');
-        $('#hoverScaleSettings').addClass('hidden');
-        
-        if (action === 'addShadow') {
-            $('#hoverShadowSettings').removeClass('hidden');
-        } else if (action === 'addGlow') {
-            $('#hoverGlowSettings').removeClass('hidden');
-        } else if (action === 'scale') {
-            $('#hoverScaleSettings').removeClass('hidden');
-        }
+        return window.adBuilderRender.propertiesUI.updateHoverActionSettings({
+            action
+        });
     }
     
     // Save interaction settings to selected element or folder
@@ -5296,3 +5265,4 @@
     }
     
 })();
+    
