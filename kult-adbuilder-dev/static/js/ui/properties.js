8d62595e08cdf59d6d9c1daaeb295415d9005ffd
Nikki Lee <nikki.lee.nikki@gmail.com>
2026-04-20 14:38:50 +0800

    Update properties.js

--- kult-adbuilder-dev/static/js/ui/properties.js
+++ kult-adbuilder-dev/static/js/ui/properties.js
@@ -114,3 +114,97 @@ export function updateTargetElementDropdowns({ elements, getElementLabel }) {
     clickSelect.val(clickValue);
     hoverSelect.val(hoverValue);
 }
+
+export function fillCommonProperties({ element }) {
+    $('#commonPropertiesGrid').removeClass('hidden');
+
+    $('#propWidth').closest('div').removeClass('hidden');
+    $('#propHeight').closest('div').removeClass('hidden');
+    $('#propX').closest('div').removeClass('hidden');
+    $('#propY').closest('div').removeClass('hidden');
+    $('#propRotation').closest('div').removeClass('hidden');
+    $('#propOpacity').closest('div').removeClass('hidden');
+
+    $('#propWidth').val(Math.round(element.width));
+    $('#propHeight').val(Math.round(element.height));
+    $('#propX').val(Math.round(element.x));
+    $('#propY').val(Math.round(element.y));
+    $('#propRotation').val(element.rotation);
+    $('#propOpacity').val(element.opacity);
+    $('#opacityValue').text(Math.round(element.opacity * 100) + '%');
+}
+
+export function fillTextProperties({ element }) {
+    $('#textProps').removeClass('hidden');
+    $('#propText').val(element.text);
+    $('#propFontFamily').val(element.fontFamily);
+    $('#propFontSize').val(element.fontSize);
+    $('#propColor').val(element.color);
+
+    $('#propBold').toggleClass('active', element.bold);
+    $('#propItalic').toggleClass('active', element.italic);
+    $('#propUnderline').toggleClass('active', element.underline);
+
+    $('.text-align-btn').removeClass('active');
+    $(`.text-align-btn[data-align="${element.textAlign}"]`).addClass('active');
+
+    $('#propTextShadowX').val(element.shadowX || 0);
+    $('#propTextShadowY').val(element.shadowY || 0);
+    $('#propTextShadowBlur').val(element.shadowBlur || 0);
+    $('#propTextShadowColor').val(element.shadowColor || '#000000');
+    $('#propTextShadowHover').prop('checked', element.shadowHover || false);
+
+    $('#propTextGlowX').val(element.glowX || 0);
+    $('#propTextGlowY').val(element.glowY || 0);
+    $('#propTextGlowBlur').val(element.glowBlur || 0);
+    $('#propTextGlowSpread').val(element.glowSpread || 0);
+    $('#propTextGlowColor').val(element.glowColor || '#ffffff');
+    $('#propTextGlowHover').prop('checked', element.glowHover || false);
+}
+
+export function fillClickthroughProperties({ element }) {
+    $('#clickthroughProps').removeClass('hidden');
+    $('#propClickUrl').val(element.url || '');
+    $('#propClickIndex').val(element.clickIndex || 1);
+    $('#propClickTarget').val(element.target || '_blank');
+    $('#imageBorderRadius').addClass('hidden');
+}
+
+export function fillShapeProperties({ element }) {
+    $('#shapeProps').removeClass('hidden');
+    $('#propShapeType').val(element.shapeType);
+    $('#propShapeColor').val(element.fillColor);
+    $('#propShapeTransparent').prop('checked', element.transparent || false);
+    $('#propShapeBorderWidth').val(element.borderWidth || 0);
+    $('#propShapeBorderColor').val(element.borderColor || '#000000');
+    $('#propShapeBorderRadius').val(element.borderRadius || 0);
+
+    $('#propShapeShadowX').val(element.shadowX || 0);
+    $('#propShapeShadowY').val(element.shadowY || 0);
+    $('#propShapeShadowBlur').val(element.shadowBlur || 0);
+    $('#propShapeShadowSpread').val(element.shadowSpread || 0);
+    $('#propShapeShadowColor').val(element.shadowColor || '#000000');
+    $('#propShapeShadowHover').prop('checked', element.shadowHover || false);
+
+    $('#propShapeGlowX').val(element.glowX || 0);
+    $('#propShapeGlowY').val(element.glowY || 0);
+    $('#propShapeGlowBlur').val(element.glowBlur || 0);
+    $('#propShapeGlowSpread').val(element.glowSpread || 0);
+    $('#propShapeGlowColor').val(element.glowColor || '#ffffff');
+    $('#propShapeGlowHover').prop('checked', element.glowHover || false);
+}
+
+export function fillVideoProperties({ element }) {
+    $('#videoProps').removeClass('hidden');
+    $('#propVideoUrl').val(element.videoUrl);
+    $('#propVideoName').val(element.videoName);
+    $('#propVideoPlayTrigger').val(element.playTrigger || 'autoplay');
+    $('#propVideoMuted').prop('checked', element.muted);
+    $('#propVideoControls').prop('checked', element.controls);
+
+    if (element.playTrigger === 'autoplay') {
+        $('#propVideoMuted').prop('disabled', true);
+    } else {
+        $('#propVideoMuted').prop('disabled', false);
+    }
+}
