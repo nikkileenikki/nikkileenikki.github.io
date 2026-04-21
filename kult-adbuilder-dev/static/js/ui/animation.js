bd0877ae9db400357e6847f7ec5bf7ea0b015e96
Nikki Lee <nikki.lee.nikki@gmail.com>
2026-04-21 09:39:49 +0800

    Add animation modal functions for UI handling

---
+++ kult-adbuilder-dev/static/js/ui/animation.js
@@ -0,0 +1,55 @@
+export function resetAnimationModal() {
+    $('#animBtnText').text('Add Animation');
+    $('#deleteAnimBtn').addClass('hidden');
+    $('#animStart').val(0);
+    $('#animDuration').val(1);
+
+    $('#animFade').val('');
+    $('#animSlide').val('');
+    $('#animZoom').val('');
+    $('#animRotate').val('');
+}
+
+export function populateAnimationModal({ anim }) {
+    $('#animFade').val('');
+    $('#animSlide').val('');
+    $('#animZoom').val('');
+    $('#animRotate').val('');
+
+    const types = anim.types || [anim.type];
+
+    types.forEach(type => {
+        if (type === 'fadeIn' || type === 'fadeOut') {
+            $('#animFade').val(type);
+        } else if (type.startsWith('slide')) {
+            $('#animSlide').val(type);
+        } else if (type === 'scaleIn' || type === 'scaleOut') {
+            $('#animZoom').val(type);
+        } else if (type.startsWith('rotate')) {
+            $('#animRotate').val(type);
+        }
+    });
+
+    $('#animStart').val(Number(anim.start).toFixed(1));
+    $('#animDuration').val(Number(anim.duration).toFixed(1));
+    $('#animEase').val(anim.ease);
+
+    $('#animBtnText').text('Update Animation');
+    $('#deleteAnimBtn').removeClass('hidden');
+}
+
+export function getSelectedAnimationTypes() {
+    const selectedTypes = [];
+
+    const fade = $('#animFade').val();
+    const slide = $('#animSlide').val();
+    const zoom = $('#animZoom').val();
+    const rotate = $('#animRotate').val();
+
+    if (fade) selectedTypes.push(fade);
+    if (slide) selectedTypes.push(slide);
+    if (zoom) selectedTypes.push(zoom);
+    if (rotate) selectedTypes.push(rotate);
+
+    return selectedTypes;
+}
