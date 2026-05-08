d1e385e79d23a359b75c45d43b11833759eb2f03
Nikki Lee <nikki.lee.nikki@gmail.com>
2026-05-08 11:53:14 +0800

    Add template registry foundation

---
+++ kult-adbuilder-dev/static/js/templates/registry.js
@@ -0,0 +1,8 @@
+window.adBuilderTemplateRegistry = [
+  {
+    id: 'carousel-kult-v1',
+    name: 'Carousel - KULT',
+    category: 'carousel',
+    basePath: 'static/js/template-library/carousel-kult-v1'
+  }
+];
