eff70d887a8daac9a528c5167b32ac29a4792acf
Nikki Lee <nikki.lee.nikki@gmail.com>
2026-05-11 15:35:24 +0800

    Update registry.js

--- kult-adbuilder-dev/static/js/templates/registry.js
+++ kult-adbuilder-dev/static/js/templates/registry.js
@@ -1,13 +1,18 @@
 window.adBuilderTemplateRegistry = [
   {
-    id: 'carousel-kult-v1',
-    name: 'Carousel - KULT',
-    category: 'carousel',
-    basePath: 'static/js/template-library/carousel-kult-v1'
-  }, {
     id: 'standard-banner-v1',
     name: 'Standard Banner',
     category: 'standard',
     basePath: 'static/js/template-library/standard-banner-v1'
+  },{
+    id: 'catfish-v1',
+    name: 'Catfish Banner',
+    category: 'catfish',
+    basePath: 'static/js/template-library/catfish-kult-v1'
+  },{
+    id: 'carousel-kult-v1',
+    name: 'Carousel - KULT',
+    category: 'carousel',
+    basePath: 'static/js/template-library/carousel-kult-v1'
   }
 ];
