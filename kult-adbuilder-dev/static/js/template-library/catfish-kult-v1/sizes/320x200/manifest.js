169c3d7fae81f4e82b50b6036d03936caaa52b7d
Nikki Lee <nikki.lee.nikki@gmail.com>
2026-05-11 15:29:17 +0800

    Add manifest.js for 320x200 ad template

---
+++ kult-adbuilder-dev/static/js/template-library/catfish-kult-v1/sizes/320x200/manifest.js
@@ -0,0 +1,20 @@
+FT.manifest({
+    "filename": "index.html",
+    "width": 320,
+    "height": 200,
+    "clickTagCount": 1,
+    "hideBrowsers": ["ie8"],
+     "expand": {
+        "fullscreen":false,
+        "width":450,
+        "height":210,
+        "indentAcross": 0,
+        "indentDown": 0
+    },
+    "videos": [
+    { "name": "video1", "ref": "232227/umobile-ultra5g-revamp-10s" }
+    ],
+    "trackingEvents": [
+      {"name": "close", "type": "standard"}
+    ]
+});
