5f3e7639c126924e08d7a1751ef25b3a841fb219
Nikki Lee <nikki.lee.nikki@gmail.com>
2026-05-11 09:37:08 +0800

    Update registry.js

--- kult-adbuilder-dev/static/js/templates/registry.js
+++ kult-adbuilder-dev/static/js/templates/registry.js
@@ -4,5 +4,10 @@ window.adBuilderTemplateRegistry = [
     name: 'Carousel - KULT',
     category: 'carousel',
     basePath: 'static/js/template-library/carousel-kult-v1'
+  }, {
+    id: 'standard-banner-v1',
+    name: 'Standard Banner',
+    category: 'standard',
+    basePath: 'static/js/template-library/standard-banner-v1'
   }
 ];
