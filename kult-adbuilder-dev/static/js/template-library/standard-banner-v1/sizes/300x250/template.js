9a19cc43ffb77f5fd0624b794f842f1e6087267c
Nikki Lee <nikki.lee.nikki@gmail.com>
2026-05-11 11:31:22 +0800

    Update template.js

--- kult-adbuilder-dev/static/js/template-library/standard-banner-v1/sizes/300x250/template.js
+++ kult-adbuilder-dev/static/js/template-library/standard-banner-v1/sizes/300x250/template.js
@@ -1,18 +1,27 @@
 function init(){
-			addEvents()
-		}
-		function addEvents() {
-			$( ".click" ).on( "click", function() {
-				click1();
-			} );
-			startAnim();
-		}
-		function startAnim() {
-			gsap.set('#container', {autoAlpha:1});
-		}
-		function click1(){
-			myFT.clickTag(1, "");
-		}	
-		window.onload = function(){
-			init();
-		}
+	addEvents()
+}
+function addEvents() {
+	$( ".click" ).on( "click", function() {
+		click1();
+	} );
+	startAnim();
+}
+function startAnim() {
+	gsap.set('#container', {autoAlpha:1});
+}
+function click1(){
+	myFT.clickTag(1, "");
+}	
+function checkFT(){
+	if (myFT.hasLoaded) {
+		init();
+	}else{	
+		setTimeout(() => {
+			checkFT()
+		}, 100);
+	}
+}
+window.onload = function(){
+	checkFT();
+}
